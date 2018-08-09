b6ecbc37d282a58190de2d00ad23d034a221645d
YJC <2016301110141@whu.edu.cn>
2018-08-09 18:30:33 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/YJCweather/android/gson/AQI.java
@@ -0,0 +1,11 @@
+package com.YJCweather.android.gson;
+
+public class AQI {
+
+    public AQICity city;
+
+    public class AQICity {
+        public String aqi;
+        public String pm25;
+    }
+}
--- app/src/main/java/com/YJCweather/android/util/ChooseAreaFragment.java
+++ app/src/main/java/com/YJCweather/android/util/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.YJCweather.android.util;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.YJCweather.android.R;
+import com.YJCweather.android.WeatherActivity;
 import com.YJCweather.android.db.City;
 import com.YJCweather.android.db.County;
 import com.YJCweather.android.db.Province;
@@ -91,6 +93,12 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if (currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id", weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
